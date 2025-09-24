8459415af2565a81951f9008d1cf7dd733916831
sooio <2350841405@qq.com>
2025-09-24 11:08:04 +0800

    correct of conditional

--- Data_lab/datalab-handout/bits.c
+++ Data_lab/datalab-handout/bits.c
@@ -209,7 +209,8 @@ int isAsciiDigit(int x) {
  *   Rating: 3
  */
 int conditional(int x, int y, int z) {
-  return 2;
+  int mask=~(!x)+1;
+  return (z&mask)|(y&(~mask));
 }
 /* 
  * isLessOrEqual - if x <= y  then return 1, else return 0 
